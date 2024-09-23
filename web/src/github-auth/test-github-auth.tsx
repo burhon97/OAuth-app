f49c4318bf3e4294ae2abba982fa89236a9801a1
burhon97 <idriskhonovdev97@gmail.com>
2024-09-23 09:32:08 +0500

    removed unused files

--- web/src/github-auth/test-github-auth.tsx
+++
@@ -1,7 +0,0 @@
-function SignInWithGitHub() {
-  const handleGitHubLogin = () => {
-    window.location.href = "http://localhost:3000/auth/github";
-  };
-
-  return <button onClick={handleGitHubLogin}>Sign in with GitHub</button>;
-}
